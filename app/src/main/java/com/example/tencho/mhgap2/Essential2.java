5850351c98cbb5615119eacf7efca366f5205cb1
tencho12 <tenzinchophel944@gmail.com>
2018-01-08 14:48:22 +0600

    essential change

--- app/src/main/java/com/example/tencho/mhgap2/Essential2.java
+++ app/src/main/java/com/example/tencho/mhgap2/Essential2.java
@@ -24,15 +24,6 @@ public class Essential2 extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -69,6 +60,8 @@ public class Essential2 extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent=new Intent(Essential2.this,HomeActivity.class);
+            startActivity(intent);
             return true;
         }
 
