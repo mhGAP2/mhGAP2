fd49c3f6694c6d3ac5b878904d143e9f31ff5a51
tencho12 <tenzinchophel944@gmail.com>
2018-01-11 12:32:47 +0600

    glossery upload 2

--- app/src/main/java/com/example/tencho/mhgap2/Glossery.java
+++ app/src/main/java/com/example/tencho/mhgap2/Glossery.java
@@ -1,12 +1,20 @@
 package com.example.tencho.mhgap2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.SearchView;
-import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -15,19 +23,23 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Glossery extends AppCompatActivity {
+public class Glossery extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     ListView lv;
     ArrayAdapter<String> adapter;
     ArrayList<String> arrayTerm;
     String []meaning;
     TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glossery);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-      meaning=getResources().getStringArray(R.array.glossary_meaning);
+        meaning=getResources().getStringArray(R.array.glossary_meaning);
         tv=(TextView)findViewById(R.id.def);
 
         lv=(ListView)findViewById(R.id.listviewterm);
@@ -47,6 +59,25 @@ public class Glossery extends AppCompatActivity {
                 tv.setText(meaning[i]);
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -70,5 +101,53 @@ public class Glossery extends AppCompatActivity {
             }
         });
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.symbols) {
+
+        } else if (id == R.id.essentials) {
+            Intent intent=new Intent(Glossery.this,Essential.class);
+            startActivity(intent);
+
+        } else if (id == R.id.mchart) {
+            Intent intent=new Intent(Glossery.this,masterc.class);
+            startActivity(intent);
+
+        }else if (id == R.id.glossery) {
+            Intent intent=new Intent(Glossery.this,Glossery.class);
+            startActivity(intent);
+
+        }
+        else if (id == R.id.contact) {
+            Intent intent=new Intent(Glossery.this,Contactus.class);
+            startActivity(intent);
+
+        }
+        else if (id == R.id.Aboutus) {
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
