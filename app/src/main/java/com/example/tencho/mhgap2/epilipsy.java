37109a764fd4b3d651d29712817773280d810497
sonamchoki <sonamc282@gmail.com>
2018-01-24 21:04:18 -0700

    management update

--- app/src/main/java/com/example/tencho/mhgap2/epilipsy.java
+++ app/src/main/java/com/example/tencho/mhgap2/epilipsy.java
@@ -1,17 +1,30 @@
 package com.example.tencho.mhgap2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class epilipsy extends AppCompatActivity {
-
+  Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_epilipsy);
+
+        btn=(Button)findViewById(R.id.epibutton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(epilipsy.this,epilipsy1.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/tencho/mhgap2/epilipsy1.java
@@ -0,0 +1,43 @@
+package com.example.tencho.mhgap2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class epilipsy1 extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_epilipsy1);
+
+
+        btn1 = (Button) findViewById(R.id.button3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(epilipsy1.this, epi_assesment.class);
+                startActivity(intent);
+            }
+        });
+        btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(epilipsy1.this, epilipsy2.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
+
