15935363a40999688791716c00176d756244cece
Tshering21 <tsheringjamtsho1996@gmail.com>
2018-01-25 16:29:34 +0530

    Little changes in the app

---
+++ app/src/main/java/com/example/tencho/mhgap2/Dementia_Management.java
@@ -0,0 +1,30 @@
+package com.example.tencho.mhgap2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Dementia_Management extends AppCompatActivity {
+Button btn1,btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dementia__management);
+        btn1=(Button)findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.protocol1);
+            }
+        });
+        btn2=(Button)findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.protocol2);
+            }
+        });
+
+    }
+}
