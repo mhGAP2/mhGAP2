d8ca2259cbd1f38c1193a99bc61cd5e0a716f2eb
tencho12 <tenzinchophel944@gmail.com>
2018-02-18 11:21:03 +0600

    cc

--- app/src/main/java/com/example/tencho/mhgap2/Bev_assesment.java
+++ app/src/main/java/com/example/tencho/mhgap2/Bev_assesment.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 public class Bev_assesment extends AppCompatActivity {
 
     private TextView mquestion;
-    private Button mtruebtn, mfalsebtn, homebtn, othbtn,othbtn2;
+    private Button mtruebtn, mfalsebtn, homebtn, othbtn;
 
     String[] YesQuestions;
     String[] NoQuestions;
@@ -28,10 +28,8 @@ public class Bev_assesment extends AppCompatActivity {
 
         homebtn = (Button) findViewById(R.id.home);
         othbtn = (Button) findViewById(R.id.oth);
-        othbtn2 = (Button) findViewById(R.id.oth2);
         homebtn.setVisibility(View.GONE);
         othbtn.setVisibility(View.GONE);
-        othbtn2.setVisibility(View.GONE);
 
 
         mtruebtn.setOnClickListener(new View.OnClickListener() {
@@ -48,43 +46,9 @@ public class Bev_assesment extends AppCompatActivity {
             }
         });
     }
-
     private void updateYesquestion()
     {
-        if(mquestion.getText().toString().equals(YesQuestions[0]))
-            mquestion.setText(YesQuestions[1]);
-        else if(mquestion.getText().toString().equals(YesQuestions[1]))
-            mquestion.setText(YesQuestions[2]);
-        else if(mquestion.getText().toString().equals(YesQuestions[2]))
-            mquestion.setText(YesQuestions[3]);
-        else if(mquestion.getText().toString().equals(YesQuestions[3]))
-            mquestion.setText(YesQuestions[4]);
-        else if(mquestion.getText().toString().equals(YesQuestions[4]))
-            mquestion.setText(YesQuestions[5]);
-        else if(mquestion.getText().toString().equals(YesQuestions[5]))
-        {
-            mquestion.setText(YesQuestions[6]);
-            exitQuest();
-        }
 
-        else if(mquestion.getText().toString().equals(YesQuestions[6]))
-            mquestion.setText(YesQuestions[7]);
-        else if(mquestion.getText().toString().equals(YesQuestions[7]))
-        {
-            mquestion.setText(YesQuestions[8]);
-            mtruebtn.setVisibility(View.GONE);
-            mfalsebtn.setVisibility(View.GONE);
-            othbtn2.setVisibility(View.VISIBLE);
-        }
-        else if(mquestion.getText().toString().equals(NoQuestions[3]))
-            mquestion.setText(YesQuestions[7]);
-        else if(mquestion.getText().toString().equals(NoQuestions[5]))
-            mquestion.setText(YesQuestions[8]);
-        else if(mquestion.getText().toString().equals(NoQuestions[2]))
-        {
-            mquestion.setText(YesQuestions[6]);
-            exitQuestoth();
-        }
 
     }
 
@@ -92,40 +56,6 @@ public class Bev_assesment extends AppCompatActivity {
         if(mquestion.getText().toString().equals(YesQuestions[0]) ||mquestion.getText().toString().equals(YesQuestions[1])||mquestion.getText().toString().equals(YesQuestions[2]))
         {
             mquestion.setText(NoQuestions[0]);
-            exitQuest();
         }
-        else if(mquestion.getText().toString().equals(YesQuestions[3]))
-            mquestion.setText(NoQuestions[2]);
-        else if(mquestion.getText().toString().equals(YesQuestions[5]))
-            mquestion.setText(NoQuestions[3]);
-        else if(mquestion.getText().toString().equals(NoQuestions[3]))
-        {
-            mquestion.setText(NoQuestions[4]);
-            exitQuest();
-        }
-        else if(mquestion.getText().toString().equals(YesQuestions[4]))
-        {
-            mquestion.setText(NoQuestions[1]);
-            exitQuest();
-        }
-        else if(mquestion.getText().toString().equals(YesQuestions[7]))
-            mquestion.setText(NoQuestions[5]);
-        else if(mquestion.getText().toString().equals(NoQuestions[5]))
-        {
-            mquestion.setText(NoQuestions[6]);
-            exitQuestoth();
-        }
-
-    }
-
-    public void exitQuest() {
-        homebtn.setVisibility(View.VISIBLE);
-        mtruebtn.setVisibility(View.GONE);
-        mfalsebtn.setVisibility(View.GONE);
-    }
-    public void exitQuestoth() {
-        othbtn.setVisibility(View.VISIBLE);
-        mtruebtn.setVisibility(View.GONE);
-        mfalsebtn.setVisibility(View.GONE);
     }
 }
